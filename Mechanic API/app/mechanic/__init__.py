c02ee9a435ef764bf053b2f265a5a9825fa46929
Justin Wold <justin.wold88@gmail.com>
2025-10-20 13:51:07 -0700

    Delete Mechanic API directory

--- Mechanic API/app/mechanic/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-mechanic_bp = Blueprint('mechanic', __name__)
-
-from app.mechanic import routes
--- Mechanic API/run.py
+++
@@ -1,9 +0,0 @@
-from app.mechanic import mechanic_bp
-from app import create_app, db
-
-app = create_app()
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
