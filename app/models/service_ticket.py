151563624f6d011bac84fc2ba3df6c13e85c61e9
justinwols88 <justin.wold88@gmail.com>
2025-11-30 18:36:55 -0800

    update

--- app/models/service_ticket.py
+++ app/models/service_ticket.py
@@ -61,13 +61,4 @@ class ServiceTicket(db.Model):
             'mechanics': [m.to_dict() for m in mechanics_list] if mechanics_list else [],
             'inventory': [i.to_dict() for i in inventory_list] if inventory_list else []
         }
-# Junction tables for many-to-many relationships
-service_mechanic = db.Table('service_mechanic',
-    db.Column('service_ticket_id', db.Integer, db.ForeignKey('service_ticket.id')),
-    db.Column('mechanic_id', db.Integer, db.ForeignKey('mechanic.id'))
-)
-
-ticket_inventory = db.Table('ticket_inventory',
-    db.Column('service_ticket_id', db.Integer, db.ForeignKey('service_ticket.id')),
-    db.Column('inventory_id', db.Integer, db.ForeignKey('inventory.id'))
-)
+    
